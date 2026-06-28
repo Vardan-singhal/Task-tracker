0cdfaa92eccd7d67c0a0e1f641cc0be6ce6305e2
Vardan Singhal <vardansinghal30@gmail.com>
2026-06-28 16:51:44 +0530

    Backend resolved

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,4 +1,4 @@
-const Task = require('../models/Task');
+const Task = require('../models/task.js');
 const { validationResult } = require('express-validator');
 
 // Get all tasks with filtering, searching, and sorting
