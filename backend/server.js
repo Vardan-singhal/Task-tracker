d4be315f8029aeb84f71a84d52ddef16879ed6ab
Vardan Singhal <vardansinghal30@gmail.com>
2026-06-28 17:07:03 +0530

    Successfully Deployed final project

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,16 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", 
+      process.env.CLIENT_URL, 
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
